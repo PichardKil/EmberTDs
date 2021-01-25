d6072ac8ac8e534222aa74a1b5a31bd49bee7635
Kilian Pichard <kil.pichard@icloud.com>
2021-01-25 11:23:32 +0100

    Add update, save, bootstrap

--- app/controllers/ex1.js
+++ app/controllers/ex1.js
@@ -2,13 +2,28 @@ import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
+const MAX = 100;
+
 export default class Ex1Controller extends Controller {
   @tracked count = 0;
-  @tracked content = 'Entrez votre texte';
+  @tracked content = '';
   @tracked info = '';
 
   @action
   clear() {
+    this.content = '';
     this.info = '';
+  }
+  @action
+  save() {
+    if (this.content.length !== 0) this.info = 'Note Sauvegardée';
+  }
+  @action
+  update() {
+    this.info = 'Note Modifiée';
+  }
+
+  get size() {
+    return MAX - this.content.length;
   }
 }
