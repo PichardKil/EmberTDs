1b6b541797e813efa6a75f03816616c0af6f1432
Kilian Pichard <kil.pichard@icloud.com>
2021-02-22 13:59:00 +0100

    Categories.Add

--- app/routes/categories/add.js
+++ app/routes/categories/add.js
@@ -1,4 +1,12 @@
 import Route from '@ember/routing/route';
+import { action } from '@ember/object';
 
 export default class CategoriesAddRoute extends Route {
+  model(){
+    return {};
+  }
+  @action save(category){
+    let categorie = this.store.createRecord('categorie',category);
+    categorie.save().then(()=>this.transitionTo('categorie'));
+  }
 }
