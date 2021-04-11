9f219b721142c0bba32a871652b673c0a5a7dbd9
Kilian <kil.pichard@icloud.com>
2021-04-11 18:39:27 +0200

    Update links

--- app/router.js
+++ app/router.js
@@ -7,9 +7,25 @@ export default class Router extends EmberRouter {
 }
 
 Router.map(function () {
-  this.route('absctractroute');
   this.route('board');
-  this.route('order', { path: 'order/:order_id' });
-  this.route('section');
-  this.route('my-orders');
+  this.route('order', {path:'order/:order_id'});
+  this.route('sections', function() {
+    this.route('index', function() {
+      this.route('add');
+      this.route('addProduct');
+      this.route('confirmDelete', {path: 'confirmDelete/:section_id'});
+    });
+
+    this.route('edit', {path: 'edit/:section_id'}, function() {
+      this.route('delete', {path: '/delete/:product_id'});
+      this.route('edit', {path: '/edit/:product_id'});
+      this.route('addProduct', {path: '/addProduct'});
+    });
+
+    this.route('delete', {path: 'delete/:section_id'});
+  });
+
+  this.route('products', function() {
+    this.route('add');
+  });
 });
--- app/routes/section.js
+++
@@ -1,8 +0,0 @@
-import Abstractroute from './abstractroute';
-
-export default class SectionRoute extends Abstractroute {
-  model() {
-    console.log(this.store.findAll('section', { include: 'products' }));
-    return this.store.findAll('section', { include: 'products' });
-  }
-}
